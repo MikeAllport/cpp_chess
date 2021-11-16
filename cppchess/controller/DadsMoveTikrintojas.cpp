b20b9b2bf1b3f8ab37383a4109453c101010cce5
Michael Allport <ma18533@essex.ac.uk>
2021-11-16 22:58:09 +0000

    small update

--- cppchess/controller/DadsMoveTikrintojas.cpp
+++ cppchess/controller/DadsMoveTikrintojas.cpp
@@ -2,7 +2,7 @@
 
 namespace Chess::Controller
 {
-    bool DadsMoveTikrintojas::IsMoveValid(const Model::Move move, MoveController& c_move) const override {
+    bool DadsMoveTikrintojas::IsMoveValid(const Model::Move move, MoveController& c_move) const {
         return false;
     }
     bool DadsMoveTikrintojas::IsColourAttacking(const Model::Move move, MoveController& c_move) const {
