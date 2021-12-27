b55c6b94d9c581c3f803050f595dae5a7195b7f7
Michael Allport <ma18533@essex.ac.uk>
2021-12-27 16:33:12 +0000

    Started with gui, added glfw and make a window

---
+++ chess/cppchess.cpp
@@ -0,0 +1,47 @@
+﻿// cppchessengine.cpp : Defines the entry point for the application.
+//
+
+#include "cppchess.h"
+
+using Vec2D = sf::Vector2f;
+using Window = sf::RenderWindow;
+
+using namespace std;
+
+sf::Texture Chess::Textures::Board = Chess::Textures::InitTexture("res/board.png");
+
+void drawBoard(Window& window)
+{
+	auto image = Chess::Textures::Board;
+	sf::Sprite sprite;
+	sprite.setTexture(image);
+	window.draw(sprite);
+}
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(800,800), "SFML Works!");
+	while(window.isOpen())
+	{
+		sf::Event event;
+		while(window.pollEvent(event))
+		{
+			if(event.type == sf::Event::Closed)
+			{
+				window.close();
+			}
+		}
+		window.clear();
+		drawBoard(window);
+		window.display();
+	}
+	cout << "Hello CMake." << endl;
+	VectorHelper<int> list;
+	list.v = { 1, 2, 3, 4, 5 };
+	auto newList = list.Filter([](int a) { return a == 1 || a == 2; });
+	for (int a : newList.v)
+	{
+		std::cout << a << endl;;
+	}
+	return 0;
+}
---
+++ chess/cppchess.h
@@ -0,0 +1,42 @@
+﻿// cppchessengine.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include "../cppchessengine/model/Piece.h"
+#include "../cppchessengine/model/Pawn.h"
+#include "../cppchessengine/model/King.h"
+#include "../cppchessengine/model/Queen.h"
+#include "../cppchessengine/model/Bishop.h"
+#include "../cppchessengine/model/Horse.h"
+#include "../cppchessengine/model/Castle.h"
+#include "../cppchessengine/model/Board.h"
+#include "../cppchessengine/model/Player.h"
+#include "../cppchessengine/utils/VectorHelper.h"
+#include "../cppchessengine/model/Move.h"
+#include "../cppchessengine/controller/MoveController.h"
+#include "../cppchessengine/controller/BoardController.h"
+#include "../cppchessengine/controller/MoveTikrintojas.h"
+
+
+namespace Chess
+{
+    class Textures
+    {
+    public:
+        static sf::Texture InitTexture(std::string fileuri) {
+            sf::Texture result;
+            if (!result.loadFromFile(fileuri))
+            {
+                throw "Error loading " + fileuri;
+            }
+            result.setSmooth(true);
+            return result;
+        }
+        static sf::Texture Board;
+    };
+}
+
+// TODO: Reference additional headers your program requires here.
